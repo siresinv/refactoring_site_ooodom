cb071ff92cb5f8080691c7305b0da78dab3478d5
Сыресин Виталий <siresin@gmail.com>
2025-07-28 19:07:25 +0300

    ADD UnitDocument ID propertie

--- DBContext/Entities/Company/UnitDocument.cs
+++ DBContext/Entities/Company/UnitDocument.cs
@@ -4,6 +4,7 @@ namespace Company.Entities
 {
     public class UnitDocument
     {
+        public Guid Id { get; set; }
         public Guid UnitId { get; set; }
         public Guid DocumentId { get; set; }
         public virtual Unit Unit { get; set; }
--- DBContext/Migrations/20250728153409_Add_ID_to_UnitDocument.Designer.cs
+++ DBContext/Migrations/20250728153409_Add_ID_to_UnitDocument.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Company.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DBContext.Migrations
 {
     [DbContext(typeof(CompanyDbContext))]
-    partial class CompanyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250728153409_Add_ID_to_UnitDocument")]
+    partial class Add_ID_to_UnitDocument
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
